7521f20919c9d97e25452f088087cff84924d991
Mikhail Blagov <vogal.mv@gmail.com>
2020-10-08 09:54:33 +0300

    Arguments print sample

---
+++ language-intro/src/org/mblagov/arguments/Arguments.java
@@ -0,0 +1,10 @@
+package org.mblagov.arguments;
+
+import java.util.Arrays;
+
+public class Arguments {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
